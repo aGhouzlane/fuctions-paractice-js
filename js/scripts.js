4bd860efe783c788490a8c65e78df550194992ba
aGhouzlane <ahmedghouzlane@gmail.com>
2021-03-22 16:51:17 -0400

    Explore string built-in functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 alert("Warning!");
 confirm("Are you sure you want continue?");
-var question = prompt("What is your name?");
+const question1 = prompt("What is your name?");
+const question2 = prompt("How old are you?");
+const question3 = prompt("What are your hobbies?");
 
-console.log(question);
+console.log(question1.concat(question2));
+console.log(question2.charAt(0));
+console.log(question3.toUpperCase());
